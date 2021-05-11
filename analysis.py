de5c2c8f4ed19b41b20515df70984e53f5ffa6c0
Peixian Wang <pw1329@log-3.nyu.cluster>
2021-05-11 13:42:19 -0400

    update with removing duplicates

--- analysis.py
+++ analysis.py
@@ -65,7 +65,11 @@ def read_outfile(outfile_name, delimiter="|", skiprows=2, split_tensor=True):
 
 def read_dfs(file1, file2, suffixes=("_df1", "_df2"), split_tensor=False):
     df1 = read_outfile(file1, split_tensor=split_tensor)
+    df1.drop_duplicates(subset="sentence", inplace=True)
+    
     df2 = read_outfile(file2, split_tensor=split_tensor)
+    df2.drop_duplicates(subset="sentence", inplace=True)
+
     combined_df = df1.merge(df2, on="sentence", suffixes=suffixes)
     return df1, df2, combined_df
 
